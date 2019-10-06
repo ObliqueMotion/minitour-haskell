c747312dcdf0b0c10c445e125ee811f3b5841e7c
Erik Nordin <erik@pop-os.localdomain>
2019-10-06 14:07:29 -0700

    e03 Print Integer Values of File

---
+++ e03/src/Lib.hs
@@ -0,0 +1,31 @@
+module Lib
+    ( compiler
+    ) where
+
+import Handler
+import Diagnostic
+import Position
+import Data.Char (ord)
+
+handler :: Handler
+handler = Handler { failures = 0, warnings = 0, diagnostics = []}
+
+displayDiagnostics :: Handler -> IO ()
+displayDiagnostics = putStrLn . show
+
+asciiValue :: Char -> String
+asciiValue = show . ord
+
+printIntegers :: String -> String
+printIntegers [] = []
+printIntegers xs = printIntegers' xs 1
+    where printIntegers' [] _ = []
+          printIntegers' (x:xs) n = "|  " ++ asciiValue x ++ end ++ printIntegers' xs (n+1)
+            where end = if 0 == n `mod` 8 then "\n" else drop (length $ asciiValue x) "        "
+
+compiler :: [String] -> IO ()
+compiler [ ]         = displayDiagnostics $ report handler (failure "Too few arguments." (position 0 0))
+compiler (x1:x2:xs)  = displayDiagnostics $ report handler (failure "Too many arguments." (position 0 0))
+compiler [fileName] = do 
+    input <- readFile $ fileName ++ ".mini"
+    putStrLn $ printIntegers input
