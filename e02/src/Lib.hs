3698de21d5150e9444221c15d2948b19471f6444
Erik Nordin <erik@pop-os.localdomain>
2019-10-07 17:08:48 -0700

    e02 Add check for file existence

--- e02/src/Lib.hs
+++ e02/src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
 import Handler
 import Diagnostic
 import Position
+import System.Directory (doesFileExist)
 
 handler :: Handler
 handler = Handler { failures = 0, warnings = 0, diagnostics = []}
@@ -13,5 +14,10 @@ compiler :: [String] -> IO ()
 compiler [ ]         = putStrLn $ show $ report handler (failure "Too few arguments." (position 0 0))
 compiler (x1:x2:xs)  = putStrLn $ show $ report handler (failure "Too many arguments." (position 0 0))
 compiler [fileName] = do 
-    input <- (readFile (fileName ++ ".mini"))
-    putStrLn input 
+    let path = fileName ++ ".mini"
+    fileExists <- doesFileExist path
+    if fileExists then do
+        input <- readFile path
+        putStrLn input
+    else
+        putStrLn $ show $ report handler (failure ("File (" ++ path ++ ") does not exist.") (position 0 0))
