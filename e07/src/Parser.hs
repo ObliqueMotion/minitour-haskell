6cddff9e29854049b4f9eb77dbc420c03bfc17a3
Erik Nordin <aeketn@gmail.com>
2019-11-03 16:09:12 -0800

    Left Associative Expression Parsing

--- e07/src/Parser.hs
+++ e07/src/Parser.hs
@@ -9,24 +9,23 @@ parseExpr :: [Token] -> (Expr, [Token])
 parseExpr = parseAdd
 
 parseAdd :: [Token] -> (Expr, [Token])
-parseAdd = parseAdd' . parseMult
-              where parseAdd' (e, (Token.Add:ts)) = let (e', tss) = parseMult ts in (AST.Add e e', tss)
-                    parseAdd' (e, (Token.Sub:ts)) = let (e', tss) = parseMult ts in (AST.Sub e e', tss)
+parseAdd = parseAdd' . parseMul
+              where parseAdd' (e, (Token.Add:ts)) = let (e', tss) = parseMul ts in parseAdd' ((AST.Add e e'), tss)
+                    parseAdd' (e, (Token.Sub:ts)) = let (e', tss) = parseMul ts in parseAdd' ((AST.Sub e e'), tss)
                     parseAdd' x = x
 
-parseMult :: [Token] -> (Expr, [Token])
-parseMult = parseMult' . parseUnary
-              where parseMult' (e, (Token.Mul:ts)) = let (e', tss) = parseUnary ts in (AST.Mul e e', tss)
-                    parseMult' (e, (Token.Div:ts)) = let (e', tss) = parseUnary ts in (AST.Div e e', tss)
-                    parseMult' x = x
+parseMul :: [Token] -> (Expr, [Token])
+parseMul = parseMul' . parseUnary
+              where parseMul' (e, (Token.Mul:ts)) = let (e', tss) = parseUnary ts in parseMul' ((AST.Mul e e'), tss)
+                    parseMul' (e, (Token.Div:ts)) = let (e', tss) = parseUnary ts in parseMul' ((AST.Div e e'), tss)
+                    parseMul' x = x
 
 parseUnary :: [Token] -> (Expr, [Token])
-parseUnary (Token.Add:ts)   = let (e, tss) = parseUnary ts in (AST.UPlus  e, tss)
-parseUnary (Token.Sub:ts)   = let (e, tss) = parseUnary ts in (AST.UMinus e, tss)
-parseUnary (Token.Not:ts)   = let (e, tss) = parseUnary ts in (AST.LNot   e, tss)
-parseUnary (Token.Tilde:ts) = let (e, tss) = parseUnary ts in (AST.BNot   e, tss)
-parseUnary x = parsePrimary x
+parseUnary (Token.Add:ts)   = let (e, tss) = parsePrimary ts in (AST.UPlus  e, tss)
+parseUnary (Token.Sub:ts)   = let (e, tss) = parsePrimary ts in (AST.UMinus e, tss)
+parseUnary (Token.Not:ts)   = let (e, tss) = parsePrimary ts in (AST.LNot   e, tss)
+parseUnary (Token.Tilde:ts) = let (e, tss) = parsePrimary ts in (AST.BNot   e, tss)
+parseUnary ts = parsePrimary ts
 
 parsePrimary :: [Token] -> (Expr, [Token])
 parsePrimary (Token.IntLit val:ts) = (Lit val, ts)
-parsePrimary ts = (Lit 5, ts)
